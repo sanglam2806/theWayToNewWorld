bcf23bd87e024549231b1704e733e3309176dec5
mitsuru <sanglam2806@gmail.com>
2025-09-15 12:03:59 +0900

    learn dsa

---
+++ zig/learning/src/dsa/MaximumNumberOfWordCanType.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("Hello Na-chan from Zig\n", .{});
+    const rs = canBeTypedWords("leet code", "lt");
+    std.debug.print("rs is {}", .{rs});
+}
+
+fn canBeTypedWords(text:[]const u8, brokenletters: []const u8) i32 {
+    var rs :i32 =1;
+    var lettersCheck = [_]bool{false} ** 26;
+
+    for(brokenletters) |letter| {
+        const index :usize = @intCast(letter - 'a');
+        lettersCheck[index] = true;
+    }
+
+    var check :bool = true;
+
+    for(text) |c| {
+        if(c == ' ') {
+            check = true;
+            rs += 1;
+        }
+        else {
+            const index :usize = @intCast(c - 'a');
+            if(lettersCheck[index] and check) {
+                check = false;
+                rs -= 1;
+            }
+        }
+    }
+
+    return rs;
+}
